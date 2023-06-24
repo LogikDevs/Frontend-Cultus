c80fad7f042eafc523b53160d55d7014eac5f22e
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-06-23 23:17:29 -0300

    push

--- FrontendCultus/src/app/register/register.component.ts
+++ FrontendCultus/src/app/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+const inputNombre = document.getElementById('nombre') as HTMLInputElement | null;
+const inputApellido = document.getElementById('apellido') as HTMLInputElement | null;
+const inputCorreo = document.getElementById('correo') as HTMLInputElement | null;
+const inputPassword = document.getElementById('password') as HTMLInputElement | null;
+const inputConfirmacionPassword = document.getElementById('password2') as HTMLInputElement | null;
+console.log(inputNombre, inputApellido, inputCorreo, inputPassword, inputConfirmacionPassword);
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
