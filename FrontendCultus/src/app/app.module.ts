8ea73add55fab69fc32dcc255b25bf257a7faaa7
Nico200505 <nicolas.moiti.2005@gmail.com>
2023-06-25 20:23:12 -0300

    login-prueba

--- FrontendCultus/src/app/app.module.ts
+++ FrontendCultus/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -24,3 +25,9 @@ import { FooterComponent } from './footer/footer.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  // otras rutas
+];
