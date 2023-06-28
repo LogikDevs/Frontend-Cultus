723405c816cc1c26b0166abeba0b2b09df34f0b7
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-06-27 21:18:44 -0300

    post avance

--- FrontendCultus/src/app/posts/posts.component.ts
+++ FrontendCultus/src/app/posts/posts.component.ts
@@ -2,20 +2,19 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PostClass } from 'src/app/posts/PostClass';
 import { urlPostsAPI } from 'src/app/app.component';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  getPostData: any;
-  error: any;
+export class PostsComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(){
-    this.http.get<any>(urlPostsAPI).subscribe(data => {
-      this.getPostData = data.total;
-    },error => this.error = error);
+    this.http.get(urlPostsAPI).subscribe(res => {
+      console.log(res);
+    })
   }
 }
