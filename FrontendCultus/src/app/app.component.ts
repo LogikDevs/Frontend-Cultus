6c40b71ea75365f8ebd8d3b1ba75898e58a4ec72
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-06-30 01:05:00 -0300

    push profile

--- FrontendCultus/src/app/app.component.ts
+++ FrontendCultus/src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 export var urlAuthenticationAPI: string = 'http://localhost:8000/api/v1/user';
 export var urlPostsAPI: string = 'xd';
+import { UserClass } from 'src/app/profile/profileClass'; 
+var userData: any;
+export var UserObj: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FrontendCultus';
+
+export class AppComponent implements OnInit{
   constructor(private http: HttpClient){};
+  ngOnInit(){
+    this.http.get(urlAuthenticationAPI+'/4').subscribe(res => {
+      userData = res;
+      UserObj = new UserClass(
+        userData.id,
+        userData.name,
+        userData.surname,
+        userData.age,
+        userData.gender,
+        userData.homeland,
+        userData.residence,
+        userData.description,
+        userData.profile_pic
+      )
+    })
+  }
+  
 }
--- FrontendCultus/src/app/profile/profile.component.ts
+++ FrontendCultus/src/app/profile/profile.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
-@Component({
+import { Component , Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserObj } from '../app.component';
+console.log(UserObj);
+ @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
+ 
 export class ProfileComponent {
-
+ 
+  @Input() UserObj: any = UserObj;
+  constructor(private http: HttpClient) {}
 }
