f85dd309a2cc3b4aefddbcfe6faf8bb49aeec1cc
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-07-06 20:58:51 -0300

    push Envio de datos opcionales a perfil pero profilepic es null.

--- FrontendCultus/src/app/datos-perfil/datos-perfil.component.ts
+++ FrontendCultus/src/app/datos-perfil/datos-perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import { PostRegisterService } from '../services/post-register.service';
+import { EditUserService } from '../services/edit-user.service';
 
 @Component({
   selector: 'app-datos-perfil',
@@ -6,13 +8,16 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./datos-perfil.component.scss']
 })
 export class DatosPerfilComponent {
+  constructor(private api: EditUserService){};
   selectedImage: string | undefined;
   @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
-
+  sendProfileData(ProfileEditData:any){
+    this.api.getEditUser(ProfileEditData);
+  }
   triggerFileInput() {
     this.fileInput.nativeElement.click();
   }
-
+  
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
     const reader = new FileReader();
--- FrontendCultus/src/app/services/authentication.service.ts
+++ FrontendCultus/src/app/services/authentication.service.ts
@@ -12,8 +12,8 @@ export class AuthenticationService {
   sendLogin(credentials: any){
     const body = {
       grant_type: "password",
-      client_id: "101",
-      client_secret: "gtu2QOMPHXNBAeEu0BSaSJhxwS4YLJy9Os1yI9Kg",
+      client_id: "102",
+      client_secret: "IJBc6ubwXtpauYEtCb8n6AShLRrfNOTsIAk7yA9Y",
       username: credentials.email,
       password: credentials.password
     }
