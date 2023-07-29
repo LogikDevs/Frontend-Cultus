b9551833edc157d63eadb7432f1d3accb836e5f4
Nico200505 <nicolas.moiti.2005@gmail.com>
2023-07-29 16:27:59 -0300

    comentarios modal

--- FrontendCultus/src/app/components/PostsFolder/singlepost/singlepost.component.ts
+++ FrontendCultus/src/app/components/PostsFolder/singlepost/singlepost.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+
+
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Post, Comment } from '../posts/post.model';
 import { VoteService } from 'src/app/services/vote.service';
 import { GetUserService } from 'src/app/services/get-user.service';
@@ -6,10 +10,20 @@ import { GetCommentsService } from 'src/app/services/get-comments.service';
 @Component({
 	selector: 'app-singlepost',
 	templateUrl: './singlepost.component.html',
-	styleUrls: ['./singlepost.component.scss']
-})
-export class SinglepostComponent {
+	styleUrls: ['./singlepost.component.scss'],
+	animations: [
+	    trigger('showHideComments', [
+			state('show', style({ height: '200px', opacity: 1 })),
+			state('hide', style({ height: '0', opacity: 0, display: 'none' })),
+			transition('show <=> hide', animate('300ms ease-in-out')),
+		  ]),
+	],
+  })	
+  export class SinglepostComponent implements OnInit {
+	showComments: boolean = false;
 	comments: Comment[];
+
+	noCommentsTemplate: any;
 	constructor(private api: GetUserService, private votes: VoteService, private api2: GetCommentsService) { }
 	@Input() author: any;
 	@Input() post: Post;
@@ -18,6 +32,7 @@ export class SinglepostComponent {
 	ngOnInit() {
 		this.getWriter();
 		this.getComments();
+	
 	}
 	getWriter() {
 		this.api.getUserFromId(this.post.fk_id_user).subscribe((res: any) => {
@@ -47,4 +62,19 @@ export class SinglepostComponent {
 			this.post.votes = res.votes;
 		});
 	}
-}
+
+	scrollOffset: number = 0;
+	containerVisible: boolean = false;
+
+	mostrarComentarios() {
+		this.showComments = true;
+	  }
+	  
+	ocultarComentarios() {
+		this.showComments = false;
+	
+	  }
+	  toggleComments() {
+		this.showComments = !this.showComments;
+	  }
+} 
