ab3a5b1cfb12513a24ec9261f3d2a72d9de4b0eb
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-07-31 21:37:05 -0300

    Push Create post con imagen

--- FrontendCultus/src/app/components/create-post/create-post.component.ts
+++ FrontendCultus/src/app/components/create-post/create-post.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CreatePostService } from '../../services/create-post.service';
-
+import { NewPostData } from './create-post.model';
 @Component({
 	selector: 'app-create-post',
 	templateUrl: './create-post.component.html',
@@ -8,15 +8,34 @@ import { CreatePostService } from '../../services/create-post.service';
 })
 export class CreatePostComponent {
 	userId = localStorage.getItem("IdUser");
-	
+	postMultimedia: File;
 	constructor(private createPostService: CreatePostService) { }
 
-	sendCreatedPost(postData: any) {
-		return this.createPostService.postCreate(postData, this.userId).subscribe((res:any)=>{
+	sendCreatedPost(FormData:any){
+		this.sendPostData(FormData);
+	}
+	sendPostData(FormData: any) {
+		const postData: NewPostData = {
+			text: FormData.text,
+			latitud: FormData.latitud,
+			longitud: FormData.longitud,
+			multimedia_file: this.postMultimedia
+		};
+		this.createPostService.postCreate(postData, this.userId).subscribe((res:any)=>{
 			console.log(res);
+			const newPostId = res.id_post;
+			if (this.postMultimedia){
+				this.sendPostMultimedia(postData.multimedia_file, newPostId)
+			}
 		});
 	}
-	sendPostMultimedia(postMultimedia:any) {
-
+	sendPostMultimedia(postMultimedia:any, id_post:any ) {
+		console.log("postMultimedia: ",postMultimedia);
+		this.createPostService.postMultimedia(postMultimedia, id_post).subscribe((res:any)=>{
+			console.log("sendPostMultimedia: ", res);
+		});
+	}
+	onFileChange(event: any) {
+		this.postMultimedia = event.target.files[0];
 	}
 }
