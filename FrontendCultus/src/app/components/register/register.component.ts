f6d84c5de29fdf8881747580bbdb2f88ed451c95
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-08-03 23:44:43 -0300

    Login error

--- FrontendCultus/src/app/components/register/register.component.ts
+++ FrontendCultus/src/app/components/register/register.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PostRegisterService } from '../../services/post-register.service';
 import { Router } from '@angular/router';
 import { StatusService } from '../../services/status.service';
 import { AuthenticationService } from '../../services/authentication.service';
 import { GetUserService } from 'src/app/services/get-user.service';
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.component.html',
 	styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+	@Input() InputError:any;
 	constructor(private getUser: GetUserService, private registerService: PostRegisterService, private router: Router, private status: StatusService, private apiauth: AuthenticationService) { };
 
 	PostRegister(inputdata: any) {
@@ -19,12 +20,23 @@ export class RegisterComponent {
 		})
 	}
 	RegisterLogin(inputdata:any){
-		this.apiauth.sendLogin(inputdata).subscribe((res2: any) => {
-			
-			localStorage.setItem('accessToken', (res2["access_token"]));
+		this.apiauth.sendLogin(inputdata).subscribe((res: any) => {
+			localStorage.setItem('accessToken', (res["access_token"]));
 			this.status.isLoggedIn = true;
-			this.router.navigateByUrl('/optionsdata');
 			this.getUser.UserIdIntoStorage();
+			this.router.navigateByUrl('/optionsdata');
+		},(error:HttpErrorResponse)=>{
+			console.log(error);
+			if (error.error && error.error.error_description) {
+				const errorDescription = error.error.error_description;
+				this.InputError = errorDescription;
+				this.ErrorReset();
+			}
 		})
 	}
+	private ErrorReset(): void {
+		setTimeout(() => {
+		  this.InputError = '';
+		}, 3000); // 3 seconds (3000 milliseconds)
+	  }
 }
--- FrontendCultus/src/app/services/authentication.service.ts
+++ FrontendCultus/src/app/services/authentication.service.ts
@@ -7,8 +7,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AuthenticationService {
 	private loginUrl = "http://localhost:8000/oauth/token";
 	private logoutUrl = "http://localhost:8000/api/v1/logout";
-	private client = "101";
-	private secret = "O5TOlV0hjR0TGsFJaVPpl7KhfMPzkEthdtTYxLUi";
+	private client = "102";
+	private secret = "OD6mY0ddiqMUGjCNAgBjC8YPaqWRCV5NogFSNxv4";
 	constructor(private http: HttpClient) { }
 
 	sendLogin(credentials: any) {
