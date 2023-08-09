49cb1b1dc0462ede7f575c90694cd6d9e344cbbb
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-08-09 10:37:29 -0300

    Edit User rehecho sin funcionar al completo

--- FrontendCultus/src/app/components/datos-perfil/datos-perfil.component.ts
+++ FrontendCultus/src/app/components/datos-perfil/datos-perfil.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { EditUserService } from '../../services/edit-user.service';
 import { GetCountriesService } from '../../services/get-countries.service';
+import { UserEditedData } from './datos-perfil.model';
+import { GetUserService } from 'src/app/services/get-user.service';
 
 @Component({
 	selector: 'app-datos-perfil',
@@ -8,31 +10,52 @@ import { GetCountriesService } from '../../services/get-countries.service';
 	styleUrls: ['./datos-perfil.component.scss']
 })
 export class DatosPerfilComponent {
+	UserData:any;
+
 	selectedImage: string | undefined;
 
 	@ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
-
-	constructor(private api: EditUserService, private countries: GetCountriesService) { };
+	ProfilePictureMultimedia: File;
+	constructor(private api: EditUserService, private countries: GetCountriesService, private userService: GetUserService) { };
 	
 	ngOnInit() {
+		this.getUser();
 		this.countriesDropbox();
 	}
-	
-	sendProfileData(ProfileEditData: any) {
-		this.api.getEditUser(ProfileEditData);
+	getUser(){
+		this.userService.getUser().subscribe((res:any)=>{
+			this.UserData = res;
+		})
 	}
-
+	sendProfileData(ProfileEditData: any) {
+		const DataToEdit: UserEditedData = {
+			description: ProfileEditData.description,
+			gender: ProfileEditData.gender,
+			homeland: ProfileEditData.homeland,
+			profile_pic: this.ProfilePictureMultimedia,
+			residence_country: ProfileEditData.residenceCountry
+		}
+		const NonNullData: Partial<UserEditedData> = {};
+		for (const key of Object.keys(DataToEdit)) {
+  			if (DataToEdit[key] !== null && DataToEdit[key] !== undefined && DataToEdit[key] !== "") {
+    			NonNullData[key] = DataToEdit[key];
+  			}
+		}
+		console.log(NonNullData);
+		this.api.getEditUser(this.UserData, NonNullData);
+		
+	}
 	triggerFileInput() {
 		this.fileInput.nativeElement.click();
 	}
 
 	onFileSelected(event: any) {
-		const file: File = event.target.files[0];
+		this.ProfilePictureMultimedia = event.target.files[0];
 		const reader = new FileReader();
 		reader.onload = (e: any) => {
 			this.selectedImage = e.target.result;
 		};
-		reader.readAsDataURL(file);
+		reader.readAsDataURL(this.ProfilePictureMultimedia);
 	}
 
 	countriesDropbox() {
--- FrontendCultus/src/app/services/edit-user.service.ts
+++ FrontendCultus/src/app/services/edit-user.service.ts
@@ -7,27 +7,32 @@ import { Router } from '@angular/router';
 	providedIn: 'root'
 })
 export class EditUserService {
-	private urlApiEditUser = "http://localhost:8000/api/v1/user/";
-	constructor(private http: HttpClient, private api: GetUserService, private router: Router) {
-	}
+	userId:any = localStorage.getItem("IdUser");
+	UserData:any;
 
-	getEditUser(EditUserData: any) {
-		this.api.getUser().subscribe((res: any) => {
-			const body = {
-				id: res.id,
-				name: res.name,
-				surname: res.surname,
-				age: res.age,
-				email: res.email,
-				password: res.password,
-				homeland: EditUserData.homeland,
-				residence: EditUserData.residenceCountry,
-				gender: EditUserData.gender,
-				profile_pic: EditUserData.profile_pic,
-				description: EditUserData.description
-			}
-			this.sendEditUser(body);
+	private urlApiEditUser = "http://localhost:8000/api/v1/user/";
+	constructor(private http: HttpClient, private userService: GetUserService, private router: Router) {}
+	
+	getEditUser(UserData:any, EditUserData: any) {
+		const body:any = {
+			email: UserData.email,
+			name: UserData.name,
+			surname: UserData.surname,
+			age: UserData.age,
+			password: UserData.password
+		}
+		const FinalBodyEditedInfo = Object.assign({}, body, EditUserData);
+		console.log(FinalBodyEditedInfo);
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ` + localStorage.getItem("accessToken")
+			})
+		}
+		this.http.put(this.urlApiEditUser + this.userId, FinalBodyEditedInfo, httpOptions).subscribe((res: any) => {
+			console.log(res);
 		});
+		//this.router.navigateByUrl('/SelectInterest');
 	}
 	sendEditUser(body: any) {
 		const httpOptions = {
