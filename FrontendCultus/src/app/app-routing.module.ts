3eb0826f5c34137d79a261cff5396579c15b40c5
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-08-18 08:10:46 -0300

    push Ingreso de datos de Evento

--- FrontendCultus/src/app/app-routing.module.ts
+++ FrontendCultus/src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ import { SidebarBackofficeComponent } from './components/sidebar-backoffice/side
 import { BackofficePostComponent } from './components/backoffice-post/backoffice-post.component';
 import { BackofficeUsersComponent } from './components/backoffice-users/backoffice-users.component';
 import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.component';
+import { CreateEventComponent } from './components/create-event/create-event.component';
 
 const routes: Routes = [ 
   { path: '', component: HomeComponent, canActivate: [GuardAuthentication]},
@@ -41,7 +42,8 @@ const routes: Routes = [
   { path: 'backoffice-post', component: BackofficePostComponent, canActivate: [GuardAuthentication] },
   { path: 'backoffice-users', component: BackofficeUsersComponent, canActivate: [GuardAuthentication]},
   { path: 'sidebar-backoffice', component: SidebarBackofficeComponent, canActivate: [GuardAuthentication] },
-  { path: 'Menu-Mobile', component: MenuMobileComponent, canActivate: [GuardAuthentication] }
+  { path: 'Menu-Mobile', component: MenuMobileComponent, canActivate: [GuardAuthentication] },
+  { path: 'createEvent', component: CreateEventComponent, canActivate: [GuardAuthentication] }
 
 ];
 
