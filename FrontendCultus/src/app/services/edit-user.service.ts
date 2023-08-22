03fbfba92acfaff2fb91511d37fd5afecba99d19
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-08-21 22:52:19 -0300

    Register 2

--- FrontendCultus/src/app/services/edit-user.service.ts
+++ FrontendCultus/src/app/services/edit-user.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { GetUserService } from './get-user.service';
-import { Router } from '@angular/router';
 
 @Injectable({
 	providedIn: 'root'
@@ -10,25 +8,24 @@ export class EditUserService {
 	userId:any = localStorage.getItem("IdUser");
 
 	private urlApiEditUser = "http://localhost:8000/api/v1/user/";
-	constructor(private http: HttpClient, private router: Router) {}
+
+	constructor(private http: HttpClient) {}
 	
 	getEditUser(EditUserData: any) {
-		console.log('EditUserData: ',EditUserData);
+
 		const formData = new FormData();
+
 		formData.append('profile_pic', EditUserData.profile_pic);
 		formData.append('homeland', EditUserData.homeland);
 		formData.append('residence', EditUserData.residence_country);
 		formData.append('gender', EditUserData.gender);
 		formData.append('description', EditUserData.description);
-		console.log('formData', formData);
+
 		const httpOptions = {
 			headers: new HttpHeaders({
 				'Authorization': `Bearer ` + localStorage.getItem("accessToken")
 			})
 		}
-		this.http.post(this.urlApiEditUser + this.userId, formData, httpOptions).subscribe((res: any) => {
-			console.log(res);
-		});
-		this.router.navigateByUrl('/SelectInterest');
+		return this.http.post(this.urlApiEditUser + this.userId, formData, httpOptions);
 	}
 }
