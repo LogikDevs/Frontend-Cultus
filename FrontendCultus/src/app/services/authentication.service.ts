b9deac14670798726a8474523960690fce4db8a2
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-09-05 20:29:15 -0300

    push

--- FrontendCultus/src/app/services/authentication.service.ts
+++ FrontendCultus/src/app/services/authentication.service.ts
@@ -8,7 +8,7 @@ export class AuthenticationService {
 	private loginUrl = "http://localhost:8000/oauth/token";
 	private logoutUrl = "http://localhost:8000/api/v1/logout";
 	private client = "101";
-	private secret = "DmzETAarWZ7QDomQCKk05hG4fWhqXr1YNlAyJt6Z";
+	private secret = "Szx4MuvtN2UDfMazdt6LbIgfm0ApqAWeibJ39bIl";
 
 	constructor(private http: HttpClient) { }
 
