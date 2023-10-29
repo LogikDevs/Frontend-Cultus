03f73098014dae7b291fd9640d31050df799a81d
Nico200505 <nicolas.moiti.2005@gmail.com>
2023-10-28 23:40:02 -0300

    mini cambios

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -8,7 +8,7 @@ export class AuthenticationService {
 	private loginUrl = "http://localhost:8000/oauth/token";
 	private logoutUrl = "http://localhost:8000/api/v1/logout";
 	private client = "101";
-	private secret = "nTk0LeLIN6f2FGBOgtFDbOaglgenuXNlxa7brTDd";
+	private secret = "DSQnFjmsmV9DOKUJkKuuDTMYLWfu2FgEhVSnluPC";
 
 
 	constructor(private http: HttpClient) { }
