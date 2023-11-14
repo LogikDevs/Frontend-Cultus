a066609fa79ee3d1744000fd192226bd466c1c8a
Vice Isi <111001905+ViceIsi@users.noreply.github.com>
2023-11-13 23:10:52 -0300

    Delete

--- src/app/common/globalVariables.ts
+++
@@ -1,6 +0,0 @@
-export class API_URLs {
-    public static AUTH: string = "http://localhost:8000/";
-    public static POSTS: string = "http://localhost:8001/"; 
-    public static GROUPS: string = "http://localhost:8002/";
-    public static EVENTS: string = "http://localhost:8003/";
-}
