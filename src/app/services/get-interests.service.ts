a52a3953f00a225ef81ce6d8ac6b58b64a7fc281
Vicente Isi <VicenteIsi.IEP@gmail.com>
2023-11-19 22:59:41 -0300

    saque el country

--- src/app/services/get-interests.service.ts
+++ src/app/services/get-interests.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Interest, UserInterests } from '../components/InterestsFolder/interest/interest.model';
-import { API_URLs } from '../common/globalVariables';
+import { API_URLs } from '../common/globalVariables.ts.example';
 
 @Injectable({
   providedIn: 'root'
--- src/app/services/get-user.service.ts
+++ src/app/services/get-user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { API_URLs } from '../common/globalVariables';
+import { API_URLs } from '../common/globalVariables.ts.example';
 @Injectable({
 	providedIn: 'root'
 })
